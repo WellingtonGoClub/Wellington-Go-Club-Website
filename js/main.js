334d610bed5bacd47f5d72cca06d982dc38afafc
chritical <cmeekin@gmail.com>
2025-04-05 20:25:08 +1300

    feat: add schedule section

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     $window.on('resize', resize);
     $('a[href^="#"]').on('click', smoothScroll);
     $(".nav-container ul li a").on('click', toggleResponsiveMenu);
+    createSchedule();
   }
 
   function smoothScroll(e) {
@@ -67,6 +68,128 @@ $(document).ready(function() {
     if (nav.checked == true) {
       nav.checked = false
     }; 
+  }
+
+  function createSchedule() {
+    const scheduleDiv = document.getElementById("schedule-container");
+    if (!scheduleDiv) return;
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('body-header');
+    h2.id = "upcoming-schedule";
+    h2.innerText = "Upcoming schedule";
+
+    scheduleDiv.appendChild(h2);
+
+    const STATUS = {
+      MEETING: 'Meeting',
+      CANCELLED: 'Cancelled',
+      SPECIAL: 'Special'
+    }
+
+    const dates = [
+      createScheduleDate('2024-12-31'),
+      createScheduleDate('2025-01-07'),
+      createScheduleDate('2025-01-14'),
+      createScheduleDate('2025-01-21'),
+      createScheduleDate('2025-01-28'),
+      createScheduleDate('2025-02-04'),
+      createScheduleDate('2025-02-11'),
+      createScheduleDate('2025-02-18'),
+      createScheduleDate('2025-02-25'),
+      createScheduleDate('2025-03-04'),
+      createScheduleDate('2025-03-11'),
+      createScheduleDate('2025-03-18'),
+      createScheduleDate('2025-03-25'),
+      createScheduleDate('2025-04-01'),
+      createScheduleDate('2025-04-08'),
+      createScheduleDate('2025-04-15'),
+      createScheduleDate('2025-04-22', STATUS.CANCELLED, 'University is closed'),
+      createScheduleDate('2025-04-29', STATUS.SPECIAL, 'Annual General Meeting<br>Playing as usual afterwards'),
+      createScheduleDate('2025-05-06'),
+      createScheduleDate('2025-05-13'),
+      createScheduleDate('2025-05-20'),
+      createScheduleDate('2025-05-27'),
+      createScheduleDate('2025-06-03'),
+      createScheduleDate('2025-06-10'),
+      createScheduleDate('2025-06-17'),
+      createScheduleDate('2025-06-24'),
+      createScheduleDate('2025-07-01'),
+      createScheduleDate('2025-07-08'),
+      createScheduleDate('2025-07-15'),
+      createScheduleDate('2025-07-22'),
+      createScheduleDate('2025-07-29'),
+      createScheduleDate('2025-08-05'),
+      createScheduleDate('2025-08-12'),
+      createScheduleDate('2025-08-19'),
+      createScheduleDate('2025-08-26'),
+      createScheduleDate('2025-09-02'),
+      createScheduleDate('2025-09-09'),
+      createScheduleDate('2025-09-16'),
+      createScheduleDate('2025-09-23'),
+      createScheduleDate('2025-09-30'),
+      createScheduleDate('2025-10-07'),
+      createScheduleDate('2025-10-14'),
+      createScheduleDate('2025-10-21'),
+      createScheduleDate('2025-10-28'),
+      createScheduleDate('2025-11-04'),
+      createScheduleDate('2025-11-11'),
+      createScheduleDate('2025-11-18'),
+      createScheduleDate('2025-11-25'),
+      createScheduleDate('2025-12-02'),
+      createScheduleDate('2025-12-09'),
+      createScheduleDate('2025-12-16'),
+      createScheduleDate('2025-12-23'),
+      createScheduleDate('2025-12-30'),
+    ]
+
+    const dateList = document.createElement('ol');
+    dateList.classList.add('schedule-list');
+    scheduleDiv.appendChild(dateList);
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    for (const date of dates) {
+      if (date.date < yesterday)
+        continue;
+
+      if (dateList.childElementCount >= 4)
+        break;
+
+      const li = document.createElement('li');
+
+      if (date.status === STATUS.CANCELLED) {
+        li.classList.add('schedule-list--cancelled');
+      } else if (date.status === STATUS.SPECIAL) {
+        li.classList.add('schedule-list--special');
+      }
+
+      const dateDiv = document.createElement('div');
+      dateDiv.classList.add('schedule-list__date');
+      dateDiv.innerHTML = date.date.toDateString();
+
+      const statusDiv = document.createElement('div');
+      statusDiv.classList.add('schedule-list__status');
+      statusDiv.innerHTML = date.status + '<br>' + date.message;
+
+      li.appendChild(dateDiv);
+      li.appendChild(statusDiv);
+      dateList.appendChild(li);
+    }
+
+    if (dateList.childElementCount > 0) {
+      // Don't show schedule if there aren't any sessions scheduled
+      scheduleDiv.removeAttribute('hidden');
+    }
+    
+    function createScheduleDate(dateString, status = STATUS.MEETING, message = '') {
+      return {
+        date: new Date(dateString),
+        status: status,
+        message: message,
+      }
+    }
   }
 
   init();
